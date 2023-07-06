78261a6a85ca5ac4288550cfe1e2996a597a6caa
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-07-06 13:19:41 +0300

    first commit

---
+++ APIApp/APIApp/Repositories/IEntities/IJWT.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace APIApp.Repositories.IEntities
+{
+    public interface IJWT
+    {
+        public string GenentateToken(ICollection<Claim> claims);
+    }
+}
