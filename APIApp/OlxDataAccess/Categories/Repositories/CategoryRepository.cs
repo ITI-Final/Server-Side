e312affb888247630a5989b04a02624f82020277
Salma Negm <99206389+SalmaNegm27@users.noreply.github.com>
2023-07-08 01:58:03 +0300

    addlayer

---
+++ APIApp/OlxDataAccess/Categories/Repositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace OlxDataAccess.Categories.Repositories
+{
+    using OlxDataAccess;
+    using OlxDataAccess.DBContext;
+    using OlxDataAccess.Models;
+
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(OLXContext context) : base(context)
+        {
+        }
+    }
+}
