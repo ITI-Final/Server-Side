80cad6af5dfee724ba9309857d34c72dbc800d3b
Mohammed Gamal <mohammed_gamal7@outlook.com>
2023-07-08 14:10:21 +0300

    update BaseRepo

--- APIApp/OlxDataAccess/BaseRepository.cs
+++ APIApp/OlxDataAccess/BaseRepository.cs
@@ -1,39 +1,41 @@
-﻿
-
-namespace OlxDataAccess
+﻿namespace OlxDataAccess
 {
     using OlxDataAccess.DBContext;
 
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        #region Fileds
         private readonly OLXContext _context;
         protected readonly DbSet<T> _dbSet;
+        #endregion
 
+        #region Constructors
         public BaseRepository(OLXContext context)
         {
             _context = context;
             _dbSet = context.Set<T>();
         }
+
+        #endregion
+
+        #region Methods
+
+        #region Get
+        public virtual async Task<IEnumerable<T>> GetAll() => await _dbSet.ToListAsync();
+
+        public virtual async Task<T> GetById(int id) => await _dbSet.FindAsync(id);
+        #endregion
+
+        #region Add
         public virtual async Task Add(T entity)
         {
             await _dbSet.AddAsync(entity);
             await _context.SaveChangesAsync();
         }
 
-        public virtual async Task DeleteById(int id)
-        {
-            var entity = await GetEntity(id);
-            if (entity == null)
-                return;
+        #endregion
 
-            _dbSet.Remove(entity);
-            await _context.SaveChangesAsync();
-        }
-
-        public virtual async Task<IEnumerable<T>> GetAll() => await _dbSet.ToListAsync();
-
-        public virtual async Task<T> GetById(int id) => await _dbSet.FindAsync(id);
-
+        #region Update
         public virtual async Task Update(int id, T entity)
         {
             if (!await IsExist(id))
@@ -44,9 +46,27 @@ namespace OlxDataAccess
             await _context.SaveChangesAsync();
         }
 
+        #endregion
+
+        #region Delete
+        public virtual async Task DeleteById(int id)
+        {
+            var entity = await GetEntity(id);
+            if (entity == null)
+                return;
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region ISExisted
         private async Task<bool> IsExist(int id) => await GetEntity(id) != null;
 
         private async Task<T> GetEntity(int id) => await _dbSet.FindAsync(id);
+        #endregion
 
+        #endregion
     }
 }
