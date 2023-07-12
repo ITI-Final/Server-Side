90bcecc19b33f6febb828ea9a443d60cc8bf4dfd
mostafa9saad <85367824+mostafa9saad@users.noreply.github.com>
2023-07-12 13:55:21 +0300

    v1 upload image

---
+++ APIApp/APIApp/Controllers/ImagesController.cs
@@ -0,0 +1,46 @@
+﻿using APIApp.DTOs.PostsDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OlxDataAccess.imagesPost.Repositories;
+
+namespace APIApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private IImagesPostRepository _imagesPostRepository;
+        private IMapper _mapper;
+
+        public ImagesController(IImagesPostRepository imagesPostRepository, IMapper mapper)
+        {
+            _imagesPostRepository = imagesPostRepository;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public async Task<ActionResult> addimage([FromForm] imagesDTO imagesDTO)
+        {
+            imagesDTO.Image = uploadImage(imagesDTO.ImageFile);
+            var image = _mapper.Map<Post_Image>(imagesDTO);
+            await _imagesPostRepository.Add(image);
+            return Ok(image);
+        }
+        #region saveImage
+        [NonAction]
+        public string uploadImage(IFormFile file)
+        {
+            var special = Guid.NewGuid().ToString();
+            string hosturl = "https://localhost:7094/";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\upload\postImages", special + "-" + file.FileName);
+            using (FileStream ms = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyToAsync(ms);
+            }
+            var filename = special + "-" + file.FileName;
+            //  return $"{filename}";
+            return Path.Combine(hosturl, @"upload\postImages", filename).ToString();
+        }
+        #endregion
+
+    }
+}
---
+++ APIApp/OlxDataAccess/imagesPost/Repositories/ImagePostRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlxDataAccess.imagesPost.Repositories
+{
+    public class ImagePostRepository : BaseRepository<Post_Image>, IImagesPostRepository
+    {
+        private OLXContext _dbContext;
+        public ImagePostRepository(OLXContext context) : base(context)
+        {
+            _dbContext = context;
+        }
+
+    }
+}
