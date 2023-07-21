11c187cf28e2fa1a78cd7200fc3577dbf66566ad
mostafa9saad <85367824+mostafa9saad@users.noreply.github.com>
2023-07-21 21:53:40 +0300

    mostafa

--- APIApp/OlxDataAccess/GlobalUsing.cs
+++ APIApp/OlxDataAccess/GlobalUsing.cs
@@ -1,6 +1,10 @@
 ﻿global using OlxDataAccess.Models;
 global using Microsoft.EntityFrameworkCore;
 global using APIApp.Services.Authentication;
+global using Microsoft.AspNetCore.Http;
+//global using Microsoft.AspNetCore.SignalR;
+global using Microsoft.EntityFrameworkCore;
+global using OlxDataAccess.BaseRepository;
 global using OlxDataAccess.DBContext;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
