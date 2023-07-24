d33c653a7c3a8a108621b88608cd6179ba8d2f95
Embact <mrmisho11@gmail.com>
2023-07-25 02:01:17 +0300

    Fix Posts Error

--- APIApp/APIApp/Controllers/PostsController.cs
+++ APIApp/APIApp/Controllers/PostsController.cs
@@ -48,146 +48,146 @@
         #endregion
 
         #region Get By Id
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Post>> GetById(int id)
-        //{
-        //    Post post = await _postsReposirory.GetById(id);
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetById(int id)
+        {
+            Post post = await _postsReposirory.GetById(id);
 
-        //    if (post == null)
-        //        return Ok(AppConstants.Response<string>(AppConstants.noContentCode, AppConstants.notContentMessage));
+            if (post == null)
+                return Ok(AppConstants.Response<string>(AppConstants.noContentCode, AppConstants.notContentMessage));
 
-        //    #region serializer for fields value
-        //    //[{ "fieldID": 3, "Choices": [1, 2] }]
-        //    //List<FieldValuesDTO> fieldvalue = JsonSerializer.Deserialize<List<FieldValuesDTO>>(post.Fields)!;
-        //    #endregion
+            //    #region serializer for fields value
+            //    //[{ "fieldID": 3, "Choices": [1, 2] }]
+            //    //List<FieldValuesDTO> fieldvalue = JsonSerializer.Deserialize<List<FieldValuesDTO>>(post.Fields)!;
+            //    #endregion
 
-        //    #region return field and it's value
-        //    //List<returnFieldDTO> f = new List<returnFieldDTO>();
-        //    //List<Field> fields = post.Cat.Fields.Where(o =>
-        //    //{
-        //    //    foreach (var item in fieldvalue)
-        //    //    {
-        //    //        if (o.Id == item.fieldID)
-        //    //        {
-        //    //            return true;
-        //    //        }
-        //    //    }
-        //    //    return false;
-        //    //}).ToList();
-        //    //foreach (var item in fields)
-        //    //{
-        //    //    List<Choice> choices = item.Choices.Where(o =>
-        //    //    {
-        //    //        foreach (var item1 in fieldvalue)
-        //    //        {
-        //    //            foreach (var item2 in item1.choices)
-        //    //            {
-        //    //                if (o.Id == item2)
-        //    //                {
-        //    //                    return true;
-        //    //                }
-        //    //            }
-        //    //        }
-        //    //        return false;
+            //    #region return field and it's value
+            //    //List<returnFieldDTO> f = new List<returnFieldDTO>();
+            //    //List<Field> fields = post.Cat.Fields.Where(o =>
+            //    //{
+            //    //    foreach (var item in fieldvalue)
+            //    //    {
+            //    //        if (o.Id == item.fieldID)
+            //    //        {
+            //    //            return true;
+            //    //        }
+            //    //    }
+            //    //    return false;
+            //    //}).ToList();
+            //    //foreach (var item in fields)
+            //    //{
+            //    //    List<Choice> choices = item.Choices.Where(o =>
+            //    //    {
+            //    //        foreach (var item1 in fieldvalue)
+            //    //        {
+            //    //            foreach (var item2 in item1.choices)
+            //    //            {
+            //    //                if (o.Id == item2)
+            //    //                {
+            //    //                    return true;
+            //    //                }
+            //    //            }
+            //    //        }
+            //    //        return false;
 
-        //    //    }).ToList();
-        //    //    List<returnChoicesDTO> c = new List<returnChoicesDTO>();
-        //    //    foreach (var item1 in choices)
-        //    //    {
-        //    //        returnChoicesDTO returnChoices = new returnChoicesDTO()
-        //    //        {
-        //    //            Id = item1.Id,
-        //    //            Label = item1.Label,
-        //    //            Label_Ar = item1.Label_Ar,
-        //    //        };
-        //    //        c.Add(returnChoices);
-        //    //    }
-        //    //    returnFieldDTO returnFieldDTO = new returnFieldDTO()
-        //    //    {
-        //    //        Field_Id = item.Id,
-        //    //        Field_Name = item.Name,
-        //    //        Field_Label = item.Label,
-        //    //        Field_Label_Ar = item.Label_Ar,
-        //    //        Choices = c,
+            //    //    }).ToList();
+            //    //    List<returnChoicesDTO> c = new List<returnChoicesDTO>();
+            //    //    foreach (var item1 in choices)
+            //    //    {
+            //    //        returnChoicesDTO returnChoices = new returnChoicesDTO()
+            //    //        {
+            //    //            Id = item1.Id,
+            //    //            Label = item1.Label,
+            //    //            Label_Ar = item1.Label_Ar,
+            //    //        };
+            //    //        c.Add(returnChoices);
+            //    //    }
+            //    //    returnFieldDTO returnFieldDTO = new returnFieldDTO()
+            //    //    {
+            //    //        Field_Id = item.Id,
+            //    //        Field_Name = item.Name,
+            //    //        Field_Label = item.Label,
+            //    //        Field_Label_Ar = item.Label_Ar,
+            //    //        Choices = c,
 
-        //    //    };
-        //    //    f.Add(returnFieldDTO);
+            //    //    };
+            //    //    f.Add(returnFieldDTO);
 
-        //    //}
-        //    #endregion
+            //    //}
+            //    #endregion
 
-        //    #region return post with with it's chocien value
-        //    //GovernoratePostDTO g = new GovernoratePostDTO()
-        //    //{
-        //    //    Id = post.Post_LocationNavigation.Governorate.Id,
-        //    //    Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
-        //    //    Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
+            //    #region return post with with it's chocien value
+            //    //GovernoratePostDTO g = new GovernoratePostDTO()
+            //    //{
+            //    //    Id = post.Post_LocationNavigation.Governorate.Id,
+            //    //    Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
+            //    //    Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
 
-        //    //};
-        //    //CityPostDTO cities = new CityPostDTO()
-        //    //{
-        //    //    City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
-        //    //    City_Name_En = post.Post_LocationNavigation.City_Name_En,
-        //    //    Id = post.Post_LocationNavigation.Id,
-        //    //    Governorate = g,
+            //    //};
+            //    //CityPostDTO cities = new CityPostDTO()
+            //    //{
+            //    //    City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
+            //    //    City_Name_En = post.Post_LocationNavigation.City_Name_En,
+            //    //    Id = post.Post_LocationNavigation.Id,
+            //    //    Governorate = g,
 
-        //    //};
-        //    #endregion
+            //    //};
+            //    #endregion
 
-        //    #region return post with with it's chocien value
-        //    GovernoratePostDTO g = new GovernoratePostDTO()
-        //    {
-        //        Id = post.Post_LocationNavigation.Governorate.Id,
-        //        Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
-        //        Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
+            //    #region return post with with it's chocien value
+            //    GovernoratePostDTO g = new GovernoratePostDTO()
+            //    {
+            //        Id = post.Post_LocationNavigation.Governorate.Id,
+            //        Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
+            //        Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
 
-        //    };
-        //    CityPostDTO cities = new CityPostDTO()
-        //    {
-        //        City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
-        //        City_Name_En = post.Post_LocationNavigation.City_Name_En,
-        //        Id = post.Post_LocationNavigation.Id,
-        //        Governorate = g,
+            //    };
+            //    CityPostDTO cities = new CityPostDTO()
+            //    {
+            //        City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
+            //        City_Name_En = post.Post_LocationNavigation.City_Name_En,
+            //        Id = post.Post_LocationNavigation.Id,
+            //        Governorate = g,
 
-        //    };
-        //    #endregion
+            //    };
+            //    #endregion
 
-        //    List<GetImagesPostDTO> i = new List<GetImagesPostDTO>();
-        //    foreach (var item in post.Post_Images)
-        //    {
-        //        item.
-        //        Id = post.Post_LocationNavigation.Governorate.Id,
-        //        Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
-        //        Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
+            //    List<GetImagesPostDTO> i = new List<GetImagesPostDTO>();
+            //    foreach (var item in post.Post_Images)
+            //    {
+            //        item.
+            //        Id = post.Post_LocationNavigation.Governorate.Id,
+            //        Governorate_Name_En = post.Post_LocationNavigation.Governorate.Governorate_Name_En,
+            //        Governorate_Name_Ar = post.Post_LocationNavigation.Governorate.Governorate_Name_Ar,
 
-        //    };
-        //    CityPostDTO cities = new CityPostDTO()
-        //    {
-        //        City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
-        //        City_Name_En = post.Post_LocationNavigation.City_Name_En,
-        //        Id = post.Post_LocationNavigation.Id,
-        //        Governorate = g,
-        //        Cat_Id = post.Cat_Id,
-        //        Id = post.Id,
-        //        Price = post.Price,
-        //        Contact_Method = post.Contact_Method,
-        //        Created_Date = post.Created_Date,
-        //        Description = post.Description,
-        //        Fields = f,
-        //        Post_LocationNavigation = cities,
-        //        Post_Image = i,
-        //        Is_Special = post.Is_Special,
-        //        Is_Visible = post.Is_Visible,
-        //        Post_Location = post.Post_Location,
-        //        Price_Type = post.Price_Type,
-        //        User_Id = post.User_Id,
-        //        Title = post.Title,
-        //        Views = post.Views
-        //    };
+            //    };
+            //    CityPostDTO cities = new CityPostDTO()
+            //    {
+            //        City_Name_Ar = post.Post_LocationNavigation.City_Name_Ar,
+            //        City_Name_En = post.Post_LocationNavigation.City_Name_En,
+            //        Id = post.Post_LocationNavigation.Id,
+            //        Governorate = g,
+            //        Cat_Id = post.Cat_Id,
+            //        Id = post.Id,
+            //        Price = post.Price,
+            //        Contact_Method = post.Contact_Method,
+            //        Created_Date = post.Created_Date,
+            //        Description = post.Description,
+            //        Fields = f,
+            //        Post_LocationNavigation = cities,
+            //        Post_Image = i,
+            //        Is_Special = post.Is_Special,
+            //        Is_Visible = post.Is_Visible,
+            //        Post_Location = post.Post_Location,
+            //        Price_Type = post.Price_Type,
+            //        User_Id = post.User_Id,
+            //        Title = post.Title,
+            //        Views = post.Views
+            //    };
 
 
-        //    return Ok(AppConstants.Response<object>(AppConstants.successCode, AppConstants.getSuccessMessage, 1, 1, 1, post));
-        //}
+            return Ok(AppConstants.Response<object>(AppConstants.successCode, AppConstants.getSuccessMessage, 1, 1, 1, post));
+        }
         #endregion
 
         #region Get User Posts
